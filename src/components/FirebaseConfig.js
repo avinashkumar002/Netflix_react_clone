2e3b3fd396d318b38f2a6e5e72b813effc2a8447
avinash singh <avinashsingh30oct@gmail.com>
2023-10-31 22:43:43 +0530

    api updated

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBin3_W6nD9o9ywbbjql4Bd0y8Ct3z_dTg",
-  authDomain: "netflix-auth-f9b6f.firebaseapp.com",
-  projectId: "netflix-auth-f9b6f",
-  storageBucket: "netflix-auth-f9b6f.appspot.com",
-  messagingSenderId: "126385604183",
-  appId: "1:126385604183:web:bb3aea86e63698fa5a8512",
-  measurementId: "G-4VPQJ42CB1"
+  apiKey: "AIzaSyADErdGNsnVGF_5LDoQV0I314fbfDByQdg",
+  authDomain: "netflix-clone-d9cfe.firebaseapp.com",
+  projectId: "netflix-clone-d9cfe",
+  storageBucket: "netflix-clone-d9cfe.appspot.com",
+  messagingSenderId: "817257625541",
+  appId: "1:817257625541:web:d857dda7eb8be33bc73dac",
+  measurementId: "G-2RFSCFPNCS"
 };
 
 // Initialize Firebase
